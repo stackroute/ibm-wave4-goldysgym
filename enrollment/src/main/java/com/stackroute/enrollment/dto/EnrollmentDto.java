f37e8ec649b4923c3cacc45f706b390b88ca08a9
abhinavfzr <abhi.king11@gmail.com>
2019-03-28 16:42:10 +0530

    abhinav

--- enrollment/src/main/java/com/stackroute/enrollment/dto/EnrollmentDto.java
+++ enrollment/src/main/java/com/stackroute/enrollment/dto/EnrollmentDto.java
@@ -1,4 +1,137 @@
 package com.stackroute.enrollment.dto;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Document
 public class EnrollmentDto {
+    @Id
+    String id;
+    String programId;
+    String firstName;
+    String lastName;
+    String email;
+    int age;
+    String gender;
+    Double Height;
+    Double Weight;
+    Double price;
+    String EnrollmentName;
+    int Duration;
+    LocalDate startDate;
+    LocalDate endDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProgramId() {
+        return programId;
+    }
+
+    public void setProgramId(String programId) {
+        this.programId = programId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Double getHeight() {
+        return Height;
+    }
+
+    public void setHeight(Double height) {
+        Height = height;
+    }
+
+    public Double getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(Double weight) {
+        Weight = weight;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getEnrollmentName() {
+        return EnrollmentName;
+    }
+
+    public void setEnrollmentName(String enrollmentName) {
+        EnrollmentName = enrollmentName;
+    }
+
+    public int getDuration() {
+        return Duration;
+    }
+
+    public void setDuration(int duration) {
+        Duration = duration;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
 }
