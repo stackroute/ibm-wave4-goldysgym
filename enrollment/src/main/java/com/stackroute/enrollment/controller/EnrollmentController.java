72efd47790a26a8ac0cf7d91baafd3dec231b0bc
abhinavfzr <abhi.king11@gmail.com>
2019-04-01 09:23:01 +0530

    abhinav

--- enrollment/src/main/java/com/stackroute/enrollment/controller/EnrollmentController.java
+++ enrollment/src/main/java/com/stackroute/enrollment/controller/EnrollmentController.java
@@ -1,7 +1,6 @@
 package com.stackroute.enrollment.controller;
 
 import com.stackroute.enrollment.domain.Enrollment;
-import com.stackroute.enrollment.dto.EnrollmentDto;
 import com.stackroute.enrollment.service.EnrollmentService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin
@@ -26,36 +24,22 @@ public class EnrollmentController
     }
 
     @PostMapping("/enrollment")
-    public ResponseEntity<?> saveEnrollment(@RequestBody EnrollmentDto enrollmentDto) {
-        Enrollment enrollment=converttoEntity(enrollmentDto);
+    public ResponseEntity<?> saveEnrollment(@RequestBody Enrollment enrollment) {
         enrollmentService.saveEnrollment(enrollment);
         return new ResponseEntity<String>("succefullly created", HttpStatus.CREATED);
     }
 
     // this method will get all Enrollments
     @GetMapping("enrollments")
-    public ResponseEntity<List<EnrollmentDto>> getALlEnrollments() {
+    public ResponseEntity<List<Enrollment>> getALlEnrollments() {
         List<Enrollment> list = enrollmentService.getALLRest();
-        List<EnrollmentDto> list1=list.stream().map(enrollment -> enrollmentToDto(enrollment))
-                .collect(Collectors.toList());
-        return new ResponseEntity<List<EnrollmentDto>>(list1,HttpStatus.OK);
+        return new ResponseEntity<List<Enrollment>>(list,HttpStatus.OK);
     }
     // this method will get Enrollment by id
     @GetMapping("enrollment/{id}")
     public ResponseEntity<?> getEnrollmentById(@PathVariable String id) {
         Enrollment enrollment= enrollmentService.findbyId(id);
-        EnrollmentDto enrollmentDto=enrollmentToDto(enrollment);
         return new ResponseEntity<Enrollment>(enrollment,HttpStatus.OK);
     }
 
-    public EnrollmentDto enrollmentToDto(Enrollment enrollment)
-    {
-        EnrollmentDto enrollmentDto=modelMapper.map(enrollment, EnrollmentDto.class);
-        return  enrollmentDto;
-    }
-    public  Enrollment converttoEntity(EnrollmentDto enrollmentDto)
-    {
-        Enrollment enrollment=modelMapper.map(enrollmentDto,Enrollment.class);
-        return  enrollment;
-    }
 }
