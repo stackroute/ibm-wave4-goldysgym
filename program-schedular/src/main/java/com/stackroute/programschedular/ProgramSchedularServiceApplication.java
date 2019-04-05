5703ee4b72d47c1d26f8354821dfed91185918fd
abhinavfzr <abhi.king11@gmail.com>
2019-04-05 12:50:11 +0530

    abhinav

---
+++ program-schedular/src/main/java/com/stackroute/programschedular/ProgramSchedularServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.programschedular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramSchedularServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramSchedularServiceApplication.class, args);
+	}
+
+}
