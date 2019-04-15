93d13bfc655906c323fc85ec3e3814eabca90bd8
ups311997 <upasanapoddar3@gmail.com>
2019-04-15 18:26:45 +0530

    trainingservice

---
+++ trainer-management-service/src/main/java/com/stackroute/trainermanagementservice/TrainerManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.trainermanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainerManagementServiceApplication.class, args);
+	}
+
+}
---
+++ trainer-management-service/src/main/java/com/stackroute/trainermanagementservice/service/TrainerService.java
@@ -0,0 +1,7 @@
+package com.stackroute.trainermanagementservice.service;
+
+import com.stackroute.trainermanagementservice.domain.Trainer;
+
+public interface TrainerService {
+    Trainer saveTrainer(Trainer trainer);
+}
