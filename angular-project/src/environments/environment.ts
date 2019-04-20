bc8cbd7ee44a6be6d75b4c7d53eae9449c74263d
saurabhsk <saurabhkumar4634@gmail.com>
2019-04-20 13:54:10 +0530

    src/environments/environment.ts updated url

--- angular-project/src/environments/environment.ts
+++ angular-project/src/environments/environment.ts
@@ -3,10 +3,10 @@
 export const environment = {
   production: false,
   
-  dbUrlOut: "http://localhost:8080/program/api/v1/programs",
-  dbUrlOut1: "http://localhost:8080/program/api/v1/program",
-  dbUrlOut2:  "http://localhost:9001/api/v1/programs/",
-  dbUrlOut3:"http://localhost:8080/subscription/api/v2/subscription",
-  dbUrlOut4:"http://localhost:9003/api/v2/subscriptions/"
+  dbUrlOut: "http://13.234.148.231:8080/program/api/v1/programs",
+  dbUrlOut1: "http://13.234.148.231:8080/program/api/v1/program",
+  dbUrlOut2:  "http://13.234.148.231:8080/program/api/v1/programs/",
+  dbUrlOut3:"http://13.234.148.231:8080/subscription/api/v2/subscription",
+  dbUrlOut4:"http://13.234.148.231:8080/subscription/api/v2/subscriptions/"
 };
 
