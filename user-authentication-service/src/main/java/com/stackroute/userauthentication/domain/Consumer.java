aee9bbbbe39cce2f41761c476a8863e290bf47b9
saurabhsk <saurabhkumar4634@gmail.com>
2019-04-20 17:17:04 +0530

    Update Consumer.java

--- user-authentication-service/src/main/java/com/stackroute/userauthentication/domain/Consumer.java
+++ user-authentication-service/src/main/java/com/stackroute/userauthentication/domain/Consumer.java
@@ -31,7 +31,7 @@ public class Consumer {
         user.setPassword(enrollment.getPassword());
         user.setCnfpassword(enrollment.getPassword());
         user.setActive(true);
-        user.setRole("ADMIN");
+        user.setRole("USER");
         userService.save(user);
     }
 }
